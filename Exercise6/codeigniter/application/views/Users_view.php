574e0954f051dadb3445875284293ed98b5bc982
Francis Peralta <faperalta2@gmail.com>
2016-11-17 10:22:39 +0800

    created exercise 6

---
+++ Exercise6/codeigniter/application/views/Users_view.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html> 
+<html lang = "en">
+ 
+   <h1><center> About Me </center></h1>
+ 
+ <center>
+<a href="test.php" target="_self">Home</a> |
+<a href="about.html" target="_self">About Me</a> |
+<a href="<?php echo base_url(); ?>index.php/users/trivia" target="_self">This or That Questions</a> |
+</center>
+
+<style>
+
+
+
+body {
+   font-family: cursive;
+}
+
+table {
+    border-collapse: collapse;
+    width: 100%;
+    font-size: 24px;
+	
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+img {
+position: fixed;
+top: 390px;
+left: 430px;
+}
+
+a:link {
+    color: blue;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: pink;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:hover {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+}
+a:active {
+    color: yellow;
+    background-color: transparent;
+    text-decoration: underline;
+}
+
+</style>
+
+</head>
+	
+<body style="background-image: url(http://localhost/dods/codeigniter/Pics/background2.jpg);">
+<center>
+<h1><a href = "<?php echo base_url(); ?>index.php/users/add_view">Create Database</a></h1>
+</center>
+      
+		
+      <table border = "1"> 
+         <?php 
+            $i = 1; 
+            echo "<tr>"; 
+            echo "<td>User Id</td>"; 
+            echo "<td>Complete Name</td>"; 
+            echo "<td>Nickname</td>"; 
+            echo "<td>Email Address</td>";
+            echo "<td>Home Address</td>";
+            echo "<td>Sex</td>"; 
+            echo "<td>Cellphone No</td>"; 
+            echo "<td>Edit</td>"; 
+            echo "<td>Delete</td>"; 
+            
+            echo "<tr>"; 
+				
+            foreach($records as $r) { 
+               echo "<tr>"; 
+               echo "<td>".$r->user_id."</td>"; 
+               echo "<td>".$r->complete_name."</td>"; 
+               echo "<td>".$r->nick."</td>"; 
+               echo "<td>".$r->eadd."</td>"; 
+               echo "<td>".$r->address."</td>"; 
+               echo "<td>".$r->sex."</td>"; 
+               echo "<td>".$r->no."</td>"; 
+               echo "<td><a href = '".base_url()."index.php/users/edit/"
+                  .$r->user_id."'>Edit</a></td>"; 
+               echo "<td><a href = '".base_url()."index.php/users/delete/"
+                  .$r->user_id."'>Delete</a></td>"; 
+               echo "<tr>"; 
+            } 
+         ?>
+      </table><br><br> 
+     <table>
+  <tr>
+    <th>Full Name</th>
+    <th>Nickname</th>
+    <th>Hobbies</th>
+    <th>Interests</th>
+    <th>Favorite Websites</th>
+  </tr>
+  <tr>
+   <td>Francis Dodi Peralta</td>
+    <td>Dodi / Dods</td>
+    <td>Playing computer and Watching series</td>
+    <td>Dogs, Movies and Anime</td>
+    <td><p><a href="https://kissanime.to">https://kissanime.to </a> </p>
+        <a href="http://boredpanda.com">http://boredpanda.com</a>
+		<tr>
+  </tr>
+</table>
+   </body>
+	
+</html>
---
+++ Exercise6/codeigniter/application/views/prefer.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+ <h1><center> This or That Questions </center></h1>
+ 
+ <center>
+<a href="test.php" target="_self">Home</a> |
+<a href="about.html" target="_self">About Me</a> |
+<a href="prefer.html" target="_self">This or That Questions</a> |
+</center>
+
+
+<style>
+
+body {
+
+   background-image : url("background2.jpg");
+   font-family: cursive;
+}
+h1 {
+
+    font-size: 24px;
+    text-align: center;
+}
+
+#dog{
+position: absolute	;
+left: 100px;
+}
+
+#laundry{
+position: absolute;
+left: 450px;
+}
+
+#roller{
+position: absolute;
+left: 800px;
+}
+
+#pancake{
+position: absolute;
+left: 100px;
+top: 500px;
+}
+
+#condo{
+position: absolute;
+left: 450px;
+top: 500px;
+}
+
+#job{
+position: absolute;
+left: 800px;
+top: 500px;
+}
+
+a:link {
+    color: blue;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:visited {
+    color: pink;
+    background-color: transparent;
+    text-decoration: none;
+}
+a:hover {
+    color: red;
+    background-color: transparent;
+    text-decoration: underline;
+}
+a:active {
+    color: yellow;
+    background-color: transparent;
+    text-decoration: underline;
+}
+</style>
+
+</head>
+<body>
+<h1 id = "prefer">Which do I prefer?</h1>
+
+<div id = "dog">
+<p>Dog or Cat?</p>
+
+<p id="demo" style="display:none"><img src = "doge.jpg" style = "width: 250px;"></p>
+<button type="button" onclick="document.getElementById('demo').style.display='block'">I prefer</button>
+</div>
+
+<div id="laundry">
+<p>Washing dishes or Doing laundry?</p>
+
+<p id="demo2" style="display:none"><img src = "laundry.png" style = "width: 250px;"></p>
+
+<button type="button" onclick="document.getElementById('demo2').style.display='block'">I prefer</button>
+</div>
+
+<div id="roller">
+<p>Ferris wheel or Roller Coaster?</p>
+
+<p id="demo3" style="display:none"><img src = "roller.jpg" style = "width: 300px;"></p>
+
+<button type="button" onclick="document.getElementById('demo3').style.display='block'">I prefer</button>
+</div>
+
+<div id="pancake">
+<p>Bacon or Pancake?</p>
+
+<p id="demo4" style="display:none"><img src = "pancake.jpg" style = "width: 250px;"></p>
+
+<button type="button" onclick="document.getElementById('demo4').style.display='block'">I prefer</button>
+</div>
+
+<div id="condo">
+<p>Condominium or House?</p>
+
+<p id="demo5" style="display:none"><img src = "condo.jpg" style = "width: 250px;"></p>
+
+<button type="button" onclick="document.getElementById('demo5').style.display='block'">I prefer</button>
+</div>
+<br>
+
+<div id="job">
+<p>Win the lottery or find your perfect job?</p>
+
+<p id="demo6" style="display:none"><img src = "job.jpg" style = "width: 250px;"></p>
+
+<button type="button" onclick="document.getElementById('demo6').style.display='block'">I prefer</button>
+</div>
