3078f75c5d0f3068174bc28bc48b8d4ae845c357
Francis Peralta <faperalta2@gmail.com>
2016-11-30 18:38:21 +0800

    added "gender" field on database

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -47,12 +47,13 @@ function delete_id(id)
  <div id="content">
     <table align="center">
     <tr>
-    <th colspan="7"><a href="add_data.php">add data here.</a></th>
+    <th colspan="8"><a href="add_data.php">add data here.</a></th>
     </tr>
     <th>First Name</th>
 	<th>Middle Name</th>
     <th>Last Name</th>
 	<th>Email</th>
+	<th>Gender</th>
     <th>Home Address</th>
 	
 	
@@ -71,6 +72,7 @@ function delete_id(id)
         <td><?php echo $row[3]; ?></td>
 		<td><?php echo $row[4]; ?></td>
 		<td><?php echo $row[5]; ?></td>
+		<td><?php echo $row[6]; ?></td>
 
   <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
