e01eac6099247e91fb7e99bebc67276b28ea4e34
Francis Peralta <faperalta2@gmail.com>
2016-11-30 19:29:19 +0800

    added "comment" field on database

--- Exercise5/index.php
+++ Exercise5/index.php
@@ -47,7 +47,7 @@ function delete_id(id)
  <div id="content">
     <table align="center">
     <tr>
-    <th colspan="10"><a href="add_data.php">add data here.</a></th>
+    <th colspan="11"><a href="add_data.php">add data here.</a></th>
     </tr>
     <th>First Name</th>
 	<th>Middle Name</th>
@@ -57,6 +57,7 @@ function delete_id(id)
 	<th>Nickname</th>
     <th>Home Address</th>
 	<th>Cellphone No.</th>
+	<th>Comment</th>
 	
 	
 	
@@ -77,6 +78,7 @@ function delete_id(id)
 		<td><?php echo $row[6]; ?></td>
 		<td><?php echo $row[7]; ?></td>
 		<td><?php echo $row[8]; ?></td>
+		<td><?php echo $row[9]; ?></td>
 
   <td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
