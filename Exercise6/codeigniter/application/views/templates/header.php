8b788d7071358d1b17bd21bb8a8669f41d93f11d
Francis Peralta <faperalta2@gmail.com>
2016-12-21 21:12:27 +0800

    deleted some text

--- Exercise6/codeigniter/application/views/templates/header.php
+++ Exercise6/codeigniter/application/views/templates/header.php
@@ -1,8 +1,8 @@
 <html>
         <head>
-                <title>CodeIgniter Tutorial</title>
+                
         </head>
         <body>
- 
-                <h1>Simple CRUD</h1>
-                <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add News</a></p>
+ <br>
+ <br>
+                <p><a href="<?php echo site_url('news'); ?>">Home</a> | <a href="<?php echo site_url('news/create'); ?>">Add Data</a></p>
