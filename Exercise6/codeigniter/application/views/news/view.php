c71ffce3ca6421993fdd94438fd49c779c81f1ee
Francis Peralta <faperalta2@gmail.com>
2016-12-21 13:28:25 +0800

    fixed error and added some files

---
+++ Exercise6/codeigniter/application/views/news/view.php
@@ -0,0 +1,8 @@
+<?php
+echo $news_item['complete_name'];
+echo $news_item['nick'];
+echo $news_item['eadd'];
+echo $news_item['address'];
+echo $news_item['sex'];
+echo $news_item['no'];
+echo $news_item['comment'];
