c8be48c0d714f8b1b5d911a351deb179e73e27a9
Francis Peralta <faperalta2@gmail.com>
2016-12-21 10:22:03 +0800

    updated database for phpmyadmin

---
+++ Exercise6/codeigniter/database.sql
@@ -0,0 +1,12 @@
+CREATE TABLE test(
+	`id` INT( 10 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    `compname` varchar(255),
+    `nick` varchar(45),
+    `eadd` varchar(255),
+    `address` varchar(255),
+    `sex` varchar(6),
+    `no` varchar(11),
+    `comment` varchar(500),
+    `slug` varchar(128) NOT NULL,
+     KEY `slug` (`slug`)
+    )ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
