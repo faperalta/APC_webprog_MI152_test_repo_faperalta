6c71f2c7c55718ca09d5c686e4bc1f67cb0be0ff
Francis Peralta <faperalta2@gmail.com>
2016-12-22 10:55:50 +0800

    added codes for view page

--- Exercise6/codeigniter/application/views/news/view.php
+++ Exercise6/codeigniter/application/views/news/view.php
@@ -1,11 +1,10 @@
 <body style="background-image: url(http://localhost/Exercise6/codeigniter/Pics/background2.jpg);">
 <?php
-echo $news_item['complete_name'];
-echo $news_item['nick'];
-echo $news_item['eadd'];
-echo $news_item['address'];
-echo $news_item['sex'];
-echo $news_item['no'];
-echo $news_item['comment'];
+echo'<h2 class = "grays">'.'<label class = "gray">'."Complete Name: ".'</label>'.$news_item['complete_name'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Nick Name: ".'</label>'.$news_item['nick'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Email Address: ".'</label>'.$news_item['eadd'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Address: ".'</label>'.$news_item['address'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Sex: ".'</label>'.$news_item['sex'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Cell No.: ".'</label>'.$news_item['no'].'</h2>' ;
+echo'<h2 class = "grays">'.'<label class = "gray">'."Comment: ".'</label>'.$news_item['comment'].'</h2>' ;
 ?>
-</body>
