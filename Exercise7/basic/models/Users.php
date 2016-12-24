1e1a5aee64102097548d5db85eb12a0c5b581faf
Francis Peralta <faperalta2@gmail.com>
2016-12-24 21:50:08 +0800

    codes of users search

---
+++ Exercise7/basic/models/Users.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "users".
+ *
+ * @property integer $user_id
+ * @property string $lastName
+ * @property string $firstName
+ * @property string $middleName
+ * @property string $email
+ * @property string $gender
+ * @property string $nickname
+ * @property string $homeAddress
+ * @property string $cellphoneNo
+ * @property string $comment
+ */
+class Users extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'users';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['lastName', 'firstName', 'email', 'gender', 'nickname', 'cellphoneNo'], 'required'],
+            [['homeAddress', 'middleName'], 'safe'],
+            [['cellphoneNo'], 'integer'],
+            [['email'], 'email'],
+            [['comment'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'lastName' => 'Last Name',
+            'firstName' => 'First Name',
+            'middleName' => 'Middle Name',
+            'email' => 'Email',
+            'gender' => 'Gender',
+            'nickname' => 'Nickname',
+            'homeAddress' => 'Home Address',
+            'cellphoneNo' => 'Cellphone No',
+            'comment' => 'Comment',
+        ];
+    }
+}
